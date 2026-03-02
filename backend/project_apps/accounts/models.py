214c340d2d166f08ea196566c90eded947a46a55
Yohannes Teferi <yteferi964@gmail.com>
2026-03-02 16:06:50 +0300

    creating custom user for use acccounts

--- backend/project_apps/accounts/models.py
+++ backend/project_apps/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from  django.contrib.auth.models import AbstractUser
 # Create your models here.
+class CustomUser(AbstractUser):
+    email=models.EmailField(unique=True)
+    USERNAME_FIELD='email'
+    full_name=models.CharField(max_length=30)
+    REQUIRED_FIELDS = []
+    
+    def __str__(self):
+        return self.email
+    
